29e98efbedb00a0dc7a5cc60f0d731c03346732c
Shah Nawaaz Nabeel Tegar <shahnawaaznabeeltegar@gmail.com>
2024-04-07 02:01:53 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,57 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Data Diri</template>
+
+    Shah Nawaaz Nabeel Tegar
+    22351084
+    4F
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Hobi</template>
+
+  Hobi Gym
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Keahlian</template>
+
+    BenchPress 130kg
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Asal</template>
+
+    Pekanbaru
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Social Media</template>
+
+    <a href="https://www.instagram.com/shahnawaaznbl">Shah Nawaaz Nabeel Tegar</a>
+  </WelcomeItem>
+</template>
